12202c18dc90783eb2aae3356ed2bde2b59c141c
Hasan Ahmed <hasan.ahmed1810@gmail.com>
2021-09-11 14:05:00 +0500

    done

---
+++ app.js
@@ -0,0 +1,95 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+
+const app = express()
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.use(express.static('public'))
+
+mongoose.connect("mongodb://localhost:27017/wikiDB", {useNewUrlParser: true, useUnifiedTopology: true })
+
+const articleSchema = new mongoose.Schema({
+    title: String,
+    content: String,
+})
+
+const Article = mongoose.model('Articles', articleSchema)
+
+app.get('/articles', function(req, res) {
+    Article.find(function(err, foundArticles){
+        if(!err) {
+            res.send(foundArticles)
+        } else {
+            res.send(err)
+        }
+    })
+})
+
+app.post('/articles', function(req, res){
+
+    const newArticle = new Article({
+        title: req.body.title,
+        content: req.body.content,
+    })
+
+    newArticle.save()
+})
+
+app.delete('/articles', function(req, res){
+    Article.deleteMany(function(err){
+        if(err){
+            res.send(err)
+        }
+    })
+})
+
+app.route('/articles/:articleTitle')
+
+.get(function(req, res){
+
+    Article.findOne({title: req.params.articleTitle}, function(err, foundArticle){
+        res.send(foundArticle)
+    })
+})
+
+.put(function(req, res){
+    Article.update(
+        {title: req.params.articleTitle},
+        {title: req.body.title, content: req.body.content},
+        {overwrite: true},
+        function(err){
+            if(err){
+                res.send(err)
+            }
+        }
+    )
+})
+
+.patch(function(req, res){
+    Article.update(
+        {title: req.params.articleTitle},
+        {$set: req.body},
+        function(err){
+            if(err){
+                res.send(err)
+            }
+        }
+    )
+})
+
+.delete(function(req, res){
+    Article.deleteOne(
+        {title: req.params.articleTitle}, 
+        function(err){
+            if(err){
+                res.send(err)
+            }
+        }
+    )
+})
+
+app.listen(3000, function () {
+    console.log('server running')
+})
